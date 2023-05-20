5df59095eb572df9b0c1b3fc4203d9ced0b8f67f
PachikarriSaiTeja <121477898+PachikarriSaiTeja@users.noreply.github.com>
2023-05-20 22:51:04 +0530

    def p(x):     c=0     for i in range(1,x+1):         if x%i==0:             c+=1     if c==2:         return True     else:         return False a = int(input()) k = list(map(int,input().split())) c = int(input()) g=0 e=0 m=[] for i in k:     if p(i) is True:         m.append(i) j = set(m) for l in j:     if l<=c:         e+=1 print(e)

---
+++ primes_less_than_k.py
@@ -0,0 +1,26 @@
+def p(x):
+    c=0
+    for i in range(1,x+1):
+        if x%i==0:
+            c+=1
+    if c==2:
+        return True
+    else:
+        return False
+a = int(input())
+k = list(map(int,input().split()))
+c = int(input())
+g=0
+e=0
+m=[]
+for i in k:
+    if p(i) is True:
+        m.append(i)
+j = set(m)
+for l in j:
+    if l<=c:
+        e+=1
+print(e)
+
+
+        
